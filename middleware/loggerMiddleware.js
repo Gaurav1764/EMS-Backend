06436c468e51bed1ad6d66ee96be65689da51d9d
GAURAV KUMAR <153047679+Gaurav1764@users.noreply.github.com>
2026-06-24 15:03:50 +0530

    Add files via upload

---
+++ middleware/loggerMiddleware.js
@@ -0,0 +1,8 @@
+const loggerMiddleware = (req, res, next) => {
+  console.log("Request Method:", req.method);
+  console.log("Request URL:", req.url);
+
+  next();
+};
+
+module.exports = loggerMiddleware;
