a042e2a30ae6036b184e50c72c93c114f720dbb2
GAURAV KUMAR <153047679+Gaurav1764@users.noreply.github.com>
2026-07-02 11:54:13 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,21 +1,13 @@
 const mongoose = require("mongoose");
-const dns = require("dns");
-
-// Force Google DNS
-dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const connectDB = async () => {
   try {
-    console.log("Using DNS:", dns.getServers());
-
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI);
 
     console.log("✅ MongoDB Connected");
-    console.log("Database:", conn.connection.name);
-    console.log("Host:", conn.connection.host);
-  } catch (error) {
+  } catch (err) {
     console.error("❌ MongoDB Connection Failed");
-    console.error(error);
+    console.error(err);
     process.exit(1);
   }
 };
